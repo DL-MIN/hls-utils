f72f89138207e1f1dafe7ae900c0e01f9bdaffd1
Lars Thoms <lars.thoms@uni-hamburg.de>
2023-05-25 10:04:16 +0200

    🚨 fixed 'errcheck: Error return values of `os.Remove` and `fd.Close` are not checked'

--- stats/fifo_file.go
+++ stats/fifo_file.go
@@ -1,85 +1,92 @@
 package stats
 
 import (
-    "bufio"
-    "golang.org/x/sys/unix"
-    . "hls-utils/logger"
-    "hls-utils/terminator"
-    "os"
-    "strings"
+	"bufio"
+	"golang.org/x/sys/unix"
+	. "hls-utils/logger"
+	"hls-utils/terminator"
+	"os"
+	"strings"
 )
 
 type FIFOFile struct {
-    Source   string
-    DataFunc func(string)
-    fd       *os.File
-    dataCh   chan string
-    errCh    chan error
+	Source   string
+	DataFunc func(string)
+	fd       *os.File
+	dataCh   chan string
+	errCh    chan error
 }
 
 func NewFIFOFile(source string) (f *FIFOFile, err error) {
-    Debugf("validate file path %s", source)
-    sourceStat, err := os.Stat(source)
-    if err != nil && !os.IsNotExist(err) {
-        return
-    } else if err == nil {
-        if sourceStat.IsDir() {
-            return nil, ErrLogIsDir(source)
-        } else if err = os.Remove(source); err != nil {
-            return
-        }
-    }
+	Debugf("validate file path %s", source)
+	sourceStat, err := os.Stat(source)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	} else if err == nil {
+		if sourceStat.IsDir() {
+			return nil, ErrLogIsDir(source)
+		} else if err = os.Remove(source); err != nil {
+			return
+		}
+	}
 
-    f = &FIFOFile{
-        Source:   source,
-        DataFunc: func(s string) { Debug(s) },
-        dataCh:   make(chan string, 128),
-        errCh:    make(chan error),
-    }
+	f = &FIFOFile{
+		Source:   source,
+		DataFunc: func(s string) { Debug(s) },
+		dataCh:   make(chan string, 128),
+		errCh:    make(chan error),
+	}
 
-    Debugf("create fifo file %s", f.Source)
-    if err = unix.Mkfifo(f.Source, 0640); err != nil {
-        return
-    }
+	Debugf("create fifo file %s", f.Source)
+	if err = unix.Mkfifo(f.Source, 0640); err != nil {
+		return
+	}
 
-    Debugf("open fifo file %s", f.Source)
-    if f.fd, err = os.OpenFile(f.Source, os.O_RDWR|unix.O_NONBLOCK, os.ModeNamedPipe); err != nil {
-        os.Remove(f.Source)
-        return
-    }
+	Debugf("open fifo file %s", f.Source)
+	if f.fd, err = os.OpenFile(f.Source, os.O_RDWR|unix.O_NONBLOCK, os.ModeNamedPipe); err != nil {
+		if err := os.Remove(f.Source); err != nil {
+			Warn(err)
+		}
+		return
+	}
 
-    err = nil
-    return
+	err = nil
+	return
 }
 
 func (f *FIFOFile) Close() {
-    f.fd.Close()
-    os.Remove(f.Source)
+	if err := f.fd.Close(); err != nil {
+		Warn(err)
+	}
+
+	if err := os.Remove(f.Source); err != nil {
+		Warn(err)
+	}
 }
 
 func (f *FIFOFile) ReadPipe() {
-    defer f.Close()
+	defer f.Close()
 
-    go func() {
-        scanner := bufio.NewScanner(f.fd)
-        defer func() {
-            f.errCh <- scanner.Err()
-        }()
-        for scanner.Scan() {
-            f.dataCh <- strings.TrimSpace(scanner.Text())
-        }
-    }()
+	go func() {
+		scanner := bufio.NewScanner(f.fd)
+		defer func() {
+			f.errCh <- scanner.Err()
+		}()
+		for scanner.Scan() {
+			f.dataCh <- strings.TrimSpace(scanner.Text())
+		}
+	}()
 
-    Debugf("stream from fifo file %s", f.Source)
-    for {
-        select {
-        case <-terminator.Signal:
-            return
-        case data := <-f.dataCh:
-            f.DataFunc(data)
-        case err := <-f.errCh:
-            Fatal(err)
-            return
-        }
-    }
+	Debugf("stream from fifo file %s", f.Source)
+	for {
+		select {
+		case <-terminator.Signal:
+			return
+		case data := <-f.dataCh:
+			f.DataFunc(data)
+		case err := <-f.errCh:
+			Fatal(err)
+			return
+		}
+	}
 }
