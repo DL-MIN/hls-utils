87961629de2378fec7d7b4f4111c8b6d13716dfa
Lars Thoms <lars.thoms@uni-hamburg.de>
2023-05-25 10:06:19 +0200

    ♻️ changed default logger functions to method expressions

--- logger/logger_default.go
+++ logger/logger_default.go
@@ -3,45 +3,18 @@ package logger
 var std = NewLogger()
 
 func Default() *Logger {
-    return std
+	return std
 }
 
-func Level() int {
-    return std.level
-}
-
-func SetLevel(level int) {
-    std.level = level
-}
-
-func Debug(v ...any) {
-    std.logWithLevel(LevelDebug, nil, v...)
-}
-
-func Debugf(format string, v ...any) {
-    std.logWithLevel(LevelDebug, &format, v...)
-}
-
-func Info(v ...any) {
-    std.logWithLevel(LevelInfo, nil, v...)
-}
-
-func Infof(format string, v ...any) {
-    std.logWithLevel(LevelInfo, &format, v...)
-}
-
-func Warn(v ...any) {
-    std.logWithLevel(LevelWarn, nil, v...)
-}
-
-func Warnf(format string, v ...any) {
-    std.logWithLevel(LevelWarn, &format, v...)
-}
-
-func Fatal(v ...any) {
-    std.logWithLevel(LevelFatal, nil, v...)
-}
-
-func Fatalf(format string, v ...any) {
-    std.logWithLevel(LevelFatal, &format, v...)
-}
+var (
+	Level    = (*std).Level
+	SetLevel = (*std).SetLevel
+	Debug    = (*std).Debug
+	Debugf   = (*std).Debugf
+	Info     = (*std).Info
+	Infof    = (*std).Infof
+	Warn     = (*std).Warn
+	Warnf    = (*std).Warnf
+	Fatal    = (*std).Fatal
+	Fatalf   = (*std).Fatalf
+)
