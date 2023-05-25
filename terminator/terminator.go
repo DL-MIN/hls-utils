f50e847bfbde97977363f0d1bc63f9a91effef20
Lars Thoms <lars.thoms@uni-hamburg.de>
2023-05-25 10:01:37 +0200

    🚨 fixed 'sigchanyzer: misuse of unbuffered os.Signal channel as argument to signal.Notify'

--- terminator/terminator.go
+++ terminator/terminator.go
@@ -1,31 +1,31 @@
 package terminator
 
 import (
-    "os"
-    "os/signal"
-    "sync"
-    "syscall"
-    "time"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 const Timeout = 3 * time.Second
 
 var (
-    WaitGroup sync.WaitGroup
-    Signal    = make(chan struct{})
+	WaitGroup sync.WaitGroup
+	Signal    = make(chan struct{})
 )
 
 func init() {
-    go func() {
+	go func() {
 
-        // Listen to interrupt and termination signals
-        signalCh := make(chan os.Signal)
-        signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
-        <-signalCh
-        close(Signal)
+		// Listen to interrupt and termination signals
+		signalCh := make(chan os.Signal, 1)
+		signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+		<-signalCh
+		close(Signal)
 
-        // Guarantee termination after specified timeout
-        <-time.After(Timeout)
-        os.Exit(143)
-    }()
+		// Guarantee termination after specified timeout
+		<-time.After(Timeout)
+		os.Exit(143)
+	}()
 }
